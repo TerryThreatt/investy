f68d3303723064460d6a7e8291be4dd5a396f714
TerryThreatt <terry.threatt@gmail.com>
2020-05-24 15:32:22 -0500

    added login/logout routes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,29 +1,53 @@
 class UserController < ApplicationController
 
     #new user signup
-    get '/signup' do
+    get "/signup" do
         if !session[:user_id]
-            erb :'users/new'
+            erb :"users/new"
         else
-            redirect to '/investments'
+            redirect to "/investments"
         end
     end
 
-    post '/users' do
+    post "/signup" do
         user = User.new(name: params[:name], email: params[:email], password: params[:password])
         if user.save
             session[:user_id] = user.id
-            redirect to '/investments'
+            redirect to "/login"
         else
-            erb :'/users/failure'
+            erb :"/users/failure"
         end
     end
 
     # login user
+    get "/login" do
+        if !session[:user_id]
+            erb :"users/login"
+        else
+            redirect to "/investments"
+        end
+    end
+
+    post "/login" do
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect to "/investments"
+        else
+            redirect to "/signup"
+        end
+    end
 
     # logout user
-
+    get "/logout" do
+        if session[:user_id] != nil
+            session.destroy
+            redirect to "/login"
+        else
+            redirect to "/"
+        end
+    end
     # update user
-
+    
     # delete user
 end
